80a6ee31c4689c2402666f7b6a2c643b0474f7ec
Yashar Akish <yashar_akish@yahoo.com>
2022-03-12 15:53:24 -0500

    seted up router

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+
+import Home from './HomePage';
+import About from './AboutPage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import Error from './ErrorPage';
+import Cart from './CartPage';
+import Checkout from './CheckoutPage';
+import PrivateRoute from './PrivateRoute';
+
+export {
+  Home,
+  About,
+  Products,
+  SingleProduct,
+  Error,
+  Cart,
+  Checkout,
+  PrivateRoute,
+};
