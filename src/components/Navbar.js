fa831b08e71e01baea46c30d9489abf051ccebde
Yashar Akish <yashar_akish@yahoo.com>
2022-03-12 18:12:32 -0500

    set up products Context to open and close side bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,9 @@ import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
+
+  const {openSidebar} = useProductsContext();
+
   return (
     <NavContainer >
       <div className="nav-center">
@@ -16,7 +19,7 @@ const Nav = () => {
           <Link to='/' >
             <img src={logo} alt="comfy sloth" />
           </Link>
-          <button type="button" className="nav-toggle"><FaBars /></button>
+          <button type="button" className="nav-toggle" onClick={openSidebar}><FaBars /></button>
         </div>
         <ul className="nav-links">
           {links.map((link)=> {
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,15 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
-  const isOpen = false;
+
+  const {isSidebarOpen, closeSidebar} = useProductsContext();
+
   return (
     <SidebarContainer >
-      <aside className={`${isOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
+      <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
         <div className="sidebar-header">
           <img src={logo} alt="comfy sloth" />
-          <button className="close-btn" type='button'>
+          <button className="close-btn" type='button' onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
@@ -23,14 +25,14 @@ const Sidebar = () => {
           {links.map((link)=>{
             return (
               <li key={link.id}>
-                <Link to={link.url} >
+                <Link to={link.url} onClick={closeSidebar}>
                   {link.text}
                 </Link>
               </li>
             )
           })}
           <li>
-            <Link to='checkout'>checkout</Link>
+            <Link to='checkout' onClick={closeSidebar}>checkout</Link>
           </li>
         </ul>
         <CartButtons />
