c11c633345cafc672364cdb2c3350135dc766537
Yashar Akish <yashar_akish@yahoo.com>
2022-03-13 19:34:35 -0400

    add to cart component: select the amount of the product

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -9,6 +9,26 @@ const AddToCart = ({ product }) => {
 
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount)=> {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    })
+  };
+  const decrease = () => {
+    setAmount((oldAmount)=> {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    })
+  };
 
   return (
     <Wrapper>
@@ -19,9 +39,11 @@ const AddToCart = ({ product }) => {
             return (
               <button
                 key={index}
-                className={`${mainColor === color ? ' active color-btn' : 'color-btn'}`}
+                className={`${
+                  mainColor === color ? ' active color-btn' : 'color-btn'
+                }`}
                 style={{ background: color }}
-                onClick={()=> setMainColor(color)}
+                onClick={() => setMainColor(color)}
               >
                 {mainColor === color ? <FaCheck /> : null}
               </button>
@@ -29,7 +51,16 @@ const AddToCart = ({ product }) => {
           })}
         </div>
       </div>
-      <div className='btn-container'></div>
+      <div className='btn-container'>
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to='/cart' className='btn'>
+          add to cart
+        </Link>
+      </div>
     </Wrapper>
   );
 };
