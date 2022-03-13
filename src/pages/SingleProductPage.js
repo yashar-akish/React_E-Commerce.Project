71202bc4c98585c17d0963c5e632919beb7ecaf8
Yashar Akish <yashar_akish@yahoo.com>
2022-03-13 16:11:09 -0400

    presented information in singleProduct page

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -51,6 +51,30 @@ const SingleProductPage = () => {
   return (
     <Wrapper >
       <PageHero title={name} product/>
+      <div className="section section-center page">
+        <Link to="/products" className="btn" >back to products page</Link>
+        <div className="product-center">
+          <ProductImages />
+          <section className="content">
+            <h2>{name}</h2>
+            <Stars />
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className='desc'>{description}</p>
+            <p className="info">
+              <span>Available : </span>
+              {stock > 0 ? 'In stock' : 'out of stock'}
+            </p>
+            <p className="info">
+              <span>SKU : </span>{sku}
+            </p>
+            <p className="info">
+              <span>Brand : </span>{company}
+            </p>
+            <hr />
+            {stock > 0 && <AddToCart />}
+          </section>
+        </div>
+      </div>
     </Wrapper>
   );
 };
