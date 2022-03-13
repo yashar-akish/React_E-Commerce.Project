eaced98102e1c16bcdc00a0df6fe9e4cc43ad2f5
Yashar Akish <yashar_akish@yahoo.com>
2022-03-13 19:06:13 -0400

    add to cart component: select the color of product

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -73,7 +73,7 @@ const SingleProductPage = () => {
               <span>Brand : </span>{company}
             </p>
             <hr />
-            {stock > 0 && <AddToCart />}
+            {stock > 0 && <AddToCart product={product}/>}
           </section>
         </div>
       </div>
