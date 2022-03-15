59a6f185afd16658190f93146391951d6b9fb4e5
Yashar Akish <yashar_akish@yahoo.com>
2022-03-14 22:46:53 -0400

    sort functions and display filtered products according to sort type

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -9,6 +9,8 @@ const Sort = () => {
     grid_view,
     setGridView,
     setListView,
+    sort,
+    updateSort,
   } = useFilterContext();
 
   return (
@@ -25,7 +27,13 @@ const Sort = () => {
       <hr />
       <form>
         <label htmlFor='sort'>sort by</label>
-        <select name='sort' id='sort' className='sort-input'>
+        <select
+          name='sort'
+          id='sort'
+          className='sort-input'
+          value={sort}
+          onChange={updateSort}
+        >
           <option value='price-lowest'>Price (lowest)</option>
           <option value='price-highest'>Price (highest)</option>
           <option value='name-a'>name (a-z)</option>
