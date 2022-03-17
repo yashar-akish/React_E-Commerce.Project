f1e23a96172d629cbacdfbf1f9d0382a45957f7c
Yashar Akish <yashar_akish@yahoo.com>
2022-03-16 23:20:42 -0400

    shipping filter and clear filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -129,7 +129,21 @@ const Filters = () => {
               value={price}
             />
           </div>
+          {/* shipping */}
+          <div className='form-control shipping'>
+            <label htmlFor='shipping'>free shipping</label>
+            <input
+              type='checkbox'
+              name='shipping'
+              id='shipping'
+              onChange={updateFilters}
+              checked={shipping}
+            />
+          </div>
         </form>
+        <button type='button' className='clear-btn' onClick={clearFilters}>
+          clear filters
+        </button>
       </div>
     </Wrapper>
   );
