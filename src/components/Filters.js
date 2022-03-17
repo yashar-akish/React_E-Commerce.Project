179cae885092afab7a53273c876e0723d9a00a73
Yashar Akish <yashar_akish@yahoo.com>
2022-03-16 22:55:34 -0400

    color filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -70,10 +70,51 @@ const Filters = () => {
               onChange={updateFilters}
               className='company'
             >
-              {companies.map((c,i)=>{
-                return <option value={c} key={i}>{c}</option>
+              {companies.map((c, i) => {
+                return (
+                  <option value={c} key={i}>
+                    {c}
+                  </option>
+                );
               })}
             </select>
+          </div>
+          {/* color */}
+          <div className='form-control'>
+            <h5>colors</h5>
+            <div className='colors'>
+              {colors.map((c, i) => {
+                if (c === 'all') {
+                  return (
+                    <button
+                      key={i}
+                      name='color'
+                      onClick={updateFilters}
+                      data-color='all'
+                      className={`${
+                        color === 'all' ? 'all-btn active' : 'all-btn'
+                      }`}
+                    >
+                      all
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={i}
+                    name='color'
+                    style={{ background: c }}
+                    className={`${
+                      color === c ? 'color-btn active' : 'color-btn'
+                    }`}
+                    data-color={c}
+                    onClick={updateFilters}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                );
+              })}
+            </div>
           </div>
         </form>
       </div>
