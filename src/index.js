093132c0cd5ce5b1884df1d50f1f5c89c4b7e541
Yashar Akish <yashar_akish@yahoo.com>
2022-03-21 20:15:20 -0400

    setup Auth0

--- src/index.js
+++ src/index.js
@@ -8,13 +8,23 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+const DOMAIN = process.env.REACT_APP_ECOMMERECE_AUTH0_DOMAIN;
+const CLIENTID = process.env.REACT_APP_ECOMMERECE_AUTH0_CLIENTID;
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain={DOMAIN}
+    clientId={CLIENTID}
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
